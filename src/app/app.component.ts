43ae5158a1ad0378b094b9d58bd81d17e9c44885
okoeee <yk27.puxcm@gmail.com>
2024-10-06 10:25:49 +0900

    create home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<h1>Hello world!</h1>`,
+  imports: [
+    RouterOutlet,
+    HomeComponent
+  ],
+  templateUrl: 'app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
