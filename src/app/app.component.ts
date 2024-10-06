a004984ce2d03c07bd6bac389be442c5c1180126
okoeee <yk27.puxcm@gmail.com>
2024-10-06 10:02:55 +0900

    fix component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  template: `<h1>Hello world!</h1>`,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-sandbox';
+  title = 'homes';
 }
